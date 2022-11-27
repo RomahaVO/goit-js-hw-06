2bc6d4cfba25cef1ab985e8969b0f9399bcae77a
RomahaVO <romahabodnaruk@icloud.com>
2022-11-27 13:31:45 +0200

    doTask02

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,17 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsLi =  document.querySelector('#ingredients');
+
+const createLi = (ingredient) => { 
+  const liElements = document.createElement('li');
+    liElements.textContent = ingredient;
+    liElements.classList.add('item');
+    return liElements;
+};
+
+const elementsLi = ingredients.map(createLi);
+ingredientsLi.append(...elementsLi);
+
+
