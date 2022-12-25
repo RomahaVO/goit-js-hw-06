60d8d4d40e70d04cf4a9c2db4e30ca4bd0f86501
RomahaVO <romahabodnaruk@icloud.com>
2022-12-26 00:15:35 +0200

    exercise10

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,44 @@
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const controls= document.getElementById("controls");
+const createBtn = controls.querySelector("[data-create]");
+const destroyBtn = controls.querySelector("[data-destroy]");
+const input = controls.querySelector("input");
+const boxes = document.getElementById("boxes");
+
+function onClick() {
+  const amount = input.valueAsNumber;
+  if (!amount) return;
+  const markup = createBoxes(amount);
+  updatePages(markup);
+}
+
+
+function createBoxes(amount) {
+  const markup = [];
+  let sizeCouter = 30 ;
+  for (let i = 0; i < amount; i ++) {
+    const element = `<div class="item" style="width: ${sizeCouter}px;
+    height: ${sizeCouter}px;
+    background: ${getRandomHexColor()}"><div>`;
+    sizeCouter += 10;
+
+    markup.push(...element);
+  }
+  return markup.join("");
+}
+
+
+function updatePages(markup = "") {
+  boxes.innerHTML = markup;
+}
+
+
+createBtn.addEventListener("click", onClick);
+destroyBtn.addEventListener("click", () => {
+  input.value = "";
+  updatePages();
+});
