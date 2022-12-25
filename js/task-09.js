e04455c9940f99c18eb8b6a3efc0ad2ebd6e643b
RomahaVO <romahabodnaruk@icloud.com>
2022-12-25 21:01:09 +0200

    exercise09

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,15 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+// console.dir(getRandomHexColor);
+const body = document.body;
+const btnChangeColor = body.querySelector(".change-color");
+const spanColor = body.querySelector(".color");
+
+btnChangeColor.addEventListener("click", changeColorBody);
+
+function changeColorBody(event) {
+  body.style.backgroundColor = getRandomHexColor();
+  spanColor.textContent = getRandomHexColor();
+  // console.dir(spanColor);
+}
