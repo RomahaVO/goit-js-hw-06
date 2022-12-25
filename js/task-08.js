3480710665b81bc1689ba49fefd97e1b2ab4fe4c
RomahaVO <romahabodnaruk@icloud.com>
2022-12-25 19:08:24 +0200

    exercise08

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,23 @@
+const refs = {
+    form: document.querySelector('.login-form'),
+    button: document.querySelector('button[type="submit"]'),
+};
+
+refs.button.addEventListener("click", onFormSubmit);
+
+function onFormSubmit () {
+    refs.form.addEventListener("submit", handleSubmit);
+};
+
+function handleSubmit (event) {
+    event.preventDefault();
+    const {
+        elements: {email , password},
+    } = event.currentTarget;
+
+    if (email.value === "" || password.value === "") {
+        alert("Всі поля повинні бути заповнені!");
+    };
+    event.currentTarget.reset();
+};
 
